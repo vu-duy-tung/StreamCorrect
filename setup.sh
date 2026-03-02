4f341d826bab581871300b4ce927810d5ea57df4
vu-duy-tung <vuduycun202@gmail.com>
2026-03-02 04:15:06 +0000

    upd: upd installation

--- setup.sh
+++ setup.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 set -e
 
-# 1. Create and activate conda environment
-conda create -n StreamCorrect python=3.10 -y
-conda activate StreamCorrect
+ENV_NAME="StreamCorrect"
+
+# 1. Create conda environment
+conda create -n "$ENV_NAME" python=3.10 -y
+
+# Initialize conda for this shell session
+CONDA_BASE=$(conda info --base)
+source "$CONDA_BASE/etc/profile.d/conda.sh"
+conda activate "$ENV_NAME"
 
 # 2. Install Python dependencies
 pip install -r requirements.txt
 
 # 3. Download model checkpoints and data assets
-pip install gdown -q
-gdown --folder "https://drive.google.com/drive/folders/11tjxuqyZsUQ7mEULIXFyzdu4UwacBYC0?usp=drive_link"
+git clone https://huggingface.co/datasets/playwithmino/StreamCorrect
+mv StreamCorrect StreamCorrect_assets
 
 # 4. Extract model checkpoint
 unzip -o StreamCorrect_assets/large-v2-zh.pt.zip
@@ -19,5 +25,5 @@ rm -rf __MACOSX
 # 5. Extract evaluation dataset
 unzip -o StreamCorrect_assets/StreamCorrect.zip -d StreamCorrect_assets/
 
-echo "Setup complete! Activate the environment with: conda activate StreamCorrect"
+echo "Setup complete! Activate the environment with: conda activate $ENV_NAME"
 
